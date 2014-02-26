4c2c37c7d170ec1cc4c1c20f2dd04e742a592ccd
Thiago Alessio <thiagoalessio@me.com>
2014-02-26 21:58:30 +0100

    Removed PHP Strict warnings

--- src/tesseract_ocr.php
+++ src/tesseract_ocr.php
@@ -1,7 +1,7 @@
 <?php
 class TesseractOCR {
 
-  function recognize($originalImage) {
+  public static function recognize($originalImage) {
     $tifImage       = TesseractOCR::convertImageToTif($originalImage);
     $configFile     = TesseractOCR::generateConfigFile(func_get_args());
     $outputFile     = TesseractOCR::executeTesseract($tifImage, $configFile);
@@ -10,13 +10,13 @@ class TesseractOCR {
     return $recognizedText;
   }
 
-  function convertImageToTif($originalImage) {
+  protected static function convertImageToTif($originalImage) {
     $tifImage = sys_get_temp_dir().'/tesseract-ocr-tif-'.rand().'.tif';
     exec("convert -colorspace gray +matte $originalImage $tifImage");
     return $tifImage;
   }
 
-  function generateConfigFile($arguments) {
+  protected static function generateConfigFile($arguments) {
     $configFile = sys_get_temp_dir().'/tesseract-ocr-config-'.rand().'.conf';
     exec("touch $configFile");
     $whitelist = TesseractOCR::generateWhitelist($arguments);
@@ -28,23 +28,25 @@ class TesseractOCR {
     return $configFile;
   }
 
-  function generateWhitelist($arguments) {
+  protected static function generateWhitelist($arguments) {
     array_shift($arguments); //first element is the image path
     $whitelist = '';
     foreach($arguments as $chars) $whitelist.= join('', (array)$chars);
     return $whitelist;
   }
 
-  function executeTesseract($tifImage, $configFile) {
+  protected static function executeTesseract($tifImage, $configFile) {
     $outputFile = sys_get_temp_dir().'/tesseract-ocr-output-'.rand();
     exec("tesseract $tifImage $outputFile nobatch $configFile 2> /dev/null");
     return $outputFile.'.txt'; //tesseract appends txt extension to output file
   }
 
-  function readOutputFile($outputFile) {
+  protected static function readOutputFile($outputFile) {
     return trim(file_get_contents($outputFile));
   }
 
-  function removeTempFiles() { array_map("unlink", func_get_args()); }
+  protected static function removeTempFiles() {
+    array_map("unlink", func_get_args());
+  }
 }
 ?>
