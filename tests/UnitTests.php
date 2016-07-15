60aade9934c4d3a0e4a4425cedf72106db9b5755
Michael Lühr <michael.luehr@gmail.com>
2016-07-15 12:25:16 +0200

    move test for exception from unit to functional tests

--- tests/UnitTests.php
+++ tests/UnitTests.php
@@ -186,15 +186,4 @@ class UnitTests extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual);
     }
-
-    /**
-     * in case of errors (e.g. non-existent files) an exception is thrown
-     * 
-     * @expectedException Exception
-     */
-    public function testCommandWithInvalidFile()
-    {
-        $actual = (new WrapTesseractOCR('image.pn'))
-            ->run();
-    }
 }
