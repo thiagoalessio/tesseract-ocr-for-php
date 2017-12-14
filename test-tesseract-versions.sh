9fcd5634df7af234040238e88070b9ce4ec0290c
thiagoalessio <thiagoalessio@me.com>
2017-12-14 02:04:04 +0100

    Test different Tesseract versions on development

---
+++ test-tesseract-versions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+TESSERACT_VERSION=3.03.02-3 vendor/bin/phpunit tests/FunctionalTests.php
+TESSERACT_VERSION=3.03.03-1 vendor/bin/phpunit tests/FunctionalTests.php
+TESSERACT_VERSION=3.04.01-4 vendor/bin/phpunit tests/FunctionalTests.php
+TESSERACT_VERSION=3.04.01-5 vendor/bin/phpunit tests/FunctionalTests.php
+TESSERACT_VERSION=3.04.01-6 vendor/bin/phpunit tests/FunctionalTests.php
