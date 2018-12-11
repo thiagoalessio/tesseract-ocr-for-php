ef4cec043bfe08f5f1e812b816c34becca9a10b3
thiagoalessio <thiagoalessio@me.com>
2018-12-11 20:23:32 +0100

    Unit test appending of extension to output file

---
+++ tests/Unit/OutputFileTest.php
@@ -0,0 +1,21 @@
+<?php namespace thiagoalessio\TesseractOCR\Tests\Unit;
+
+use thiagoalessio\TesseractOCR\Tests\Common\TestCase;
+use thiagoalessio\TesseractOCR\Command;
+
+class OutputFileTest extends TestCase
+{
+	public function beforeEach()
+	{
+		$this->cmd = new Command('image', '/path/to/output/file');
+	}
+
+	public function testTxt()
+	{
+		foreach (['digits', 'quiet', 'txt', 'anything', 'else'] as $ext) {
+			$this->cmd->configFile = $ext;
+			$expected = "/path/to/output/file.txt";
+			$this->assertEquals($expected, $this->cmd->getOutputFile());
+		}
+	}
+}
